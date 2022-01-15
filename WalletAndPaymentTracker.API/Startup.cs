ccd9a80ea728b7491c8c6349c6e4aa613fa251fc
Abdallah Awartani <abdullah.y.awartani@gmail.com>
2022-01-15 16:35:04 +0200

    add angular project

--- WalletAndPaymentTracker.API/Startup.cs
+++ WalletAndPaymentTracker.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,38 +45,38 @@ namespace WalletAndPaymentTracker.API
 
             services.AddMvc();
 
-            //services.AddSwaggerGen(swagger =>
-            //{
-            //    swagger.SwaggerDoc("v1", new OpenApiInfo
-            //    {
-            //        Version = "v1",
-            //        Title = "WalletAndPaymentTrackerAPIs",
-            //        Description = "ASP.NET 5.0 Web API"
-            //    });
-            //    //swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-            //    //{
-            //    //    Name = "Authorization",
-            //    //    Type = SecuritySchemeType.ApiKey,
-            //    //    Scheme = "Bearer",
-            //    //    BearerFormat = "JWT",
-            //    //    In = ParameterLocation.Header,
-            //    //    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-            //    //});
-            //    //swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-            //    //{
-            //    //    {
-            //    //          new OpenApiSecurityScheme
-            //    //            {
-            //    //                Reference = new OpenApiReference
-            //    //                {
-            //    //                    Type = ReferenceType.SecurityScheme,
-            //    //                    Id = "Bearer"
-            //    //                }
-            //    //            },
-            //    //            new string[] {}
-            //    //    }
-            //    //});
-            //});
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WalletAndPaymentTrackerAPIs",
+                    Description = "ASP.NET 5.0 Web API"
+                });
+                //swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                //{
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.ApiKey,
+                //    Scheme = "Bearer",
+                //    BearerFormat = "JWT",
+                //    In = ParameterLocation.Header,
+                //    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                //});
+                //swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //          new OpenApiSecurityScheme
+                //            {
+                //                Reference = new OpenApiReference
+                //                {
+                //                    Type = ReferenceType.SecurityScheme,
+                //                    Id = "Bearer"
+                //                }
+                //            },
+                //            new string[] {}
+                //    }
+                //});
+            });
 
         }
 
